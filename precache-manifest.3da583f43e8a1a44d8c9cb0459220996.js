0deb13b95de886966c11b36deef8b8ba8ac9088e
Jamar-Mitchell <jamar-mitchell@hotmail.com>
2019-09-26 00:39:29 -0400

    Updates

---
+++ precache-manifest.3da583f43e8a1a44d8c9cb0459220996.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b80132844604e5c3df0e4ceea12b3aa",
+    "url": "/portfolio/public/index.html/index.html"
+  },
+  {
+    "revision": "ddd1cc8f23f5c1a890de",
+    "url": "/portfolio/public/index.html/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "7128f21711a5d9b786b7",
+    "url": "/portfolio/public/index.html/static/css/main.1cc54e50.chunk.css"
+  },
+  {
+    "revision": "ddd1cc8f23f5c1a890de",
+    "url": "/portfolio/public/index.html/static/js/2.1a0bf55e.chunk.js"
+  },
+  {
+    "revision": "7128f21711a5d9b786b7",
+    "url": "/portfolio/public/index.html/static/js/main.c8dc1e12.chunk.js"
+  },
+  {
+    "revision": "b6c3ab46ce915ed7337c",
+    "url": "/portfolio/public/index.html/static/js/runtime~main.fdd8fc0f.js"
+  },
+  {
+    "revision": "ed5c899d959b9b8a34b79ad65eafe6fa",
+    "url": "/portfolio/public/index.html/static/media/cookhelper.ed5c899d.png"
+  },
+  {
+    "revision": "9d54c5c90798b243c8b80a8dc6476535",
+    "url": "/portfolio/public/index.html/static/media/materialspec.9d54c5c9.png"
+  },
+  {
+    "revision": "42a4ee854b7588a088ba31c7dc428aa3",
+    "url": "/portfolio/public/index.html/static/media/me.42a4ee85.png"
+  },
+  {
+    "revision": "189b0668153e1358ec196ea2d2d43c88",
+    "url": "/portfolio/public/index.html/static/media/tapattend.189b0668.png"
+  },
+  {
+    "revision": "13646829a84a5785ddba9b3a7b56623c",
+    "url": "/portfolio/public/index.html/static/media/white.13646829.png"
+  }
+]);
